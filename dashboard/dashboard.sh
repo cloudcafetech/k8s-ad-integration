f30a369208c4e0afa2c4ca0adac6142af8a40bee
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2023-12-10 08:02:20 +0530

    Update dashboard.sh

--- dashboard/dashboard.sh
+++ dashboard/dashboard.sh
@@ -2,15 +2,23 @@
 showtoken=1
 cmd="kubectl proxy"
 count=`pgrep -cf "$cmd"`
-dashboard_yaml="https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml"
+#dashboard_yaml="https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml"
+dashboard_yaml="https://raw.githubusercontent.com/cloudcafetech/k8s-ad-integration/main/dashboard/dashboard-ui.yaml"
+readonly_yaml="https://raw.githubusercontent.com/cloudcafetech/k8s-ad-integration/main/dashboard/dashboard-read-only.yaml"
+admin_yaml="https://raw.githubusercontent.com/cloudcafetech/k8s-ad-integration/main/dashboard/dashboard-admin.yaml"
+ing_yaml="https://raw.githubusercontent.com/cloudcafetech/k8s-ad-integration/main/dashboard/dashboard-ing.yaml"
 msgstarted="-e Kubernetes Dashboard e[92mstartede[0m"
 msgstopped="Kubernetes Dashboard stopped"
 
 case $1 in
 start)
+   wget $dashboard_yaml
+   wget $readonly_yaml
+   wget $admin_yaml   
    kubectl apply -f $dashboard_yaml >/dev/null 2>&1
-   kubectl apply -f ~/dashboard/dashboard-admin.yaml >/dev/null 2>&1
-   kubectl apply -f ~/dashboard/dashboard-read-only.yaml >/dev/null 2>&1
+   kubectl apply -f $readonly_yaml >/dev/null 2>&1
+   kubectl apply -f $admin_yaml >/dev/null 2>&1
+   kubectl apply -f $ing_yaml >/dev/null 2>&1   
 
    if [ $count = 0 ]; then
       nohup $cmd >/dev/null 2>&1 &
@@ -26,8 +34,9 @@ stop)
       kill -9 $(pgrep -f "$cmd")
    fi
    kubectl delete -f $dashboard_yaml >/dev/null 2>&1
-   kubectl delete -f ~/dashboard/dashboard-admin.yaml >/dev/null 2>&1
-   kubectl delete -f ~/dashboard/dashboard-read-only.yaml >/dev/null 2>&1
+   kubectl delete -f $readonly_yaml >/dev/null 2>&1
+   kubectl delete -f $admin_yaml >/dev/null 2>&1
+   kubectl delete -f $ing_yaml >/dev/null 2>&1      
    echo $msgstopped
    ;;
 
@@ -60,23 +69,3 @@ if [ $showtoken -gt 0 ]; then
    kubectl get secret -n kubernetes-dashboard $(kubectl get serviceaccount read-only-user -n kubernetes-dashboard -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" | base64 --decode
    echo
 fi
-
-
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: admin-user
-  namespace: kubernetes-dashboard
----
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: admin-user
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: cluster-admin
-subjects:
-- kind: ServiceAccount
-  name: admin-user
-  namespace: kubernetes-dashboard
