583cc62a7f653ff8e01849913b844cb052760261
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2023-12-12 08:08:32 +0530

    Create run-ldap.sh

---
+++ run-ldap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Run LDAP Server
+
+apt install ldap-utils -y
+HIP=`ip -o -4 addr list enp1s0 | awk '{print $4}' | cut -d/ -f1`
+
+docker run --name ldap-server -p 389:389 -p 636:636 \
+--env LDAP_TLS_VERIFY_CLIENT=try \
+--env LDAP_ORGANISATION="Cloudcafe Org" \
+--env LDAP_DOMAIN="cloudcafe.org" \
+--env LDAP_ADMIN_PASSWORD="StrongAdminPassw0rd" \
+--detach osixia/openldap:latest
+
+# Check LDAP Server UP & Running
+
+until [ $(docker inspect -f "{{json .State.Status }}" $(docker ps -a | grep ldap-server | awk '{print $1}')) == '"running"' ]; do echo "Waiting for LDAP to UP..." && sleep 1; done
+
+# Add LDAP User & Group
+
+wget -q https://raw.githubusercontent.com/cloudcafetech/k8s-ad-integration/main/ldap-records.ldif
+ldapadd -x -H ldap://$HIP -D "cn=admin,dc=cloudcafe,dc=org" -w StrongAdminPassw0rd -f ldap-records.ldif
+
+# LDAP query (Verify)
+
+ldapsearch -x -H ldap://$HIP -D "cn=admin,dc=cloudcafe,dc=org" -b "dc=cloudcafe,dc=org" -w "StrongAdminPassw0rd"
