e1e46e4740c92ebc010c2f5ba1dc9ba7a8bf3e48
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2023-12-17 22:12:13 +0530

    Update ad-integration.sh

--- ad-integration.sh
+++ ad-integration.sh
@@ -49,6 +49,11 @@ sed -i -e "s|172.30.2.2|$PUBIPM|g" oauth-proxy.yaml
 sed -i -e "s|172.30.1.2|$PUBIPN|g" oauth-proxy.yaml
 kubectl create -f oauth-proxy.yaml
 
+# Gangway Deployment
+wget -q https://raw.githubusercontent.com/cloudcafetech/k8s-ad-integration/main/gangway.yaml
+sed -i -e "s|172.30.1.2|$PUBIPM|g" gangway.yaml
+kubectl create -f gangway.yaml
+
 # Create the role binding for different users
 kubectl create rolebinding titli-view-default --clusterrole=view --user=titlikar@cloudcafe.org -n default
 kubectl create rolebinding rajat-admin-default --clusterrole=admin --user=rajatkar@cloudcafe.org -n default
