290cccce18da17f8ee647c4eb98582149725a202
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2023-12-18 07:12:10 +0530

    Update ad-integration.sh

--- ad-integration.sh
+++ ad-integration.sh
@@ -14,8 +14,8 @@ WORKERIP=172.168.5.1
 wget -q https://raw.githubusercontent.com/cloudcafetech/k8s-ad-integration/main/certgen.sh
 sed -i -e "s|172.30.2.2|$PUBIPM|g" certgen.sh
 sed -i -e "s|172.30.1.2|$PUBIPN|g" certgen.sh
-sed -i -e "s|oauth-proxy.172.30.1.2.nip.io|auth.$MASTERIP.nip.io|g" certgen.sh
-sed -i -e "s|oauth-proxy.172.30.2.2.nip.io|auth.$WORKERIP.nip.io|g" certgen.sh
+sed -i -e "s|oauth-proxy.$PUBIPM.nip.io|auth.$MASTERIP.nip.io|g" certgen.sh
+sed -i -e "s|oauth-proxy.$PUBIPN.nip.io|auth.$WORKERIP.nip.io|g" certgen.sh
 chmod 755 certgen.sh
 ./certgen.sh
 
