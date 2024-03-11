2400c1b71f6664a53be5d47501a2ad5dcd712f01
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2024-03-11 08:09:05 +0530

    Update new-user-ldap.sh

--- new-user-ldap.sh
+++ new-user-ldap.sh
@@ -10,7 +10,9 @@ ldapsearch -x -H ldap://$LDAPIP -D "cn=admin,dc=cloudcafe,dc=org" -b "dc=cloudca
 # Download LDAP new user ldif file & insert in LDAP
 echo "Adding new user in LDAP .."
 wget -q https://raw.githubusercontent.com/cloudcafetech/k8s-ad-integration/main/new-add-user.ldif
+wget -q https://raw.githubusercontent.com/cloudcafetech/k8s-ad-integration/main/add-user-in-grp.ldif
 ldapadd -x -H ldap://$LDAPIP -D "cn=admin,dc=cloudcafe,dc=org" -w StrongAdminPassw0rd -f new-add-user.ldif
+ldapadd -x -H ldap://$LDAPIP -D "cn=admin,dc=cloudcafe,dc=org" -w StrongAdminPassw0rd -f add-user-in-grp.ldif
 
 # After adding number of records in LDAP
 echo "After adding number of records in LDAP"
